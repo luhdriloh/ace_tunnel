3793fb313ab999313d9ef1f90cb3b93dc480e98a
luhdriloh <007.sam.moore@gmail.com>
2018-11-14 20:08:58 -0700

    Added level select scene

---
+++ Assets/Scripts/SceneManagers/LevelSelectSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSelectSceneManager : MonoBehaviour
+{
+    public Button _levelOne;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _levelOne.onClick.AddListener(PlayGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene("GamePlayScene");
+    }
+
+}
